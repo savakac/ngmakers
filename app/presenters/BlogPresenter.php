580cd091f155fd32c3868324f6498b169aa052fb
Jan Salva <jan.salva@me.com>
2014-11-26 19:59:32 +0100

    Add files

--- app/presenters/BlogPresenter.php
+++ app/presenters/BlogPresenter.php
@@ -13,17 +13,17 @@ class BlogPresenter extends BasePresenter
 {
 
 	private $database;
+	private $limitTable;
 
 	public function __construct(Nette\Database\Context $database)
 	{
 		$this->database = $database;
+		$this->limitTable = 1;
 	}
 
 	public function renderDefault()
 	{
-		$this->template->blog_contents = $this->database->table('blog_content')->order('create_post DESC')->limit(5);
-
-		$this->template->anyVariable = 'any value';
+		$this->template->blog_contents = $this->database->table('blog_content')->order('create_post DESC')->limit($this->limitTable);
 	}
 
 }
