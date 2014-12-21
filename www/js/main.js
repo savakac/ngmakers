61fd0cb0888089106bc9d0f2f4d7abe9e3153d95
Jan Salva <jan.salva@me.com>
2014-12-21 20:19:30 +0100

    Edit page code

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,27 @@
+// Pridanie triedy active pre rodicov s odkazom a
+$(document).ready(function(){
+	$("#uvod a:contains('uvod')").parent().addClass("active");
+
+});
+
 $(function(){
 
 });
+
+function activClassPage() {
+	var active = document.getElementById("title");
+
+	if (active == "Úvod") {
+		$(document).ready(function(){
+			$("#active1").addClass("active");
+		});
+	} else if (active == "Blog") {
+		$(document).ready(function(){
+			$("#active2").addClass("active");
+		});
+	} else {
+		$(document).ready(function(){
+			$("#active2").addClass("active");
+		});
+	}
+}
