2882f1d1fefcdc24cda0ab0e1ba3700026111871
Jan Salva <jan.salva@me.com>
2015-01-04 15:40:17 +0100

    Add and edit content blog

--- app/presenters/BlogPresenter.php
+++ app/presenters/BlogPresenter.php
@@ -21,9 +21,69 @@ class BlogPresenter extends BasePresenter
 		$this->limitTable = 7;
 	}
 
+	// Zobrazenie prispevku na stranke blogu
 	public function renderDefault()
 	{
 		$this->template->blog_contents = $this->database->table('blog_content')->order('create_post DESC')->limit($this->limitTable);
+	}
+
+	// Zobrazenie prispevku na stranke blogu s id
+	public function renderShow($id)
+	{
+		$contents = $this->database->table('blog_content')->get($id);
+		if (!$contents) {
+			$this->error("Stranka nebola najdena.");
+		}
+		$this->template->contents = $contents;
+	}
+
+	// Vytvorenie formularu pre vkladanie dalsich clankou
+	protected function createComponentPostForm()
+	{
+		if (!$this->user->isLoggedIn()) {
+			$this->error('Pre pridavanie alebo editovanie prispevku musite byt prihlaseny.');
+		} else {
+			$form = new Nette\Application\UI\Form;
+
+			$form->addText('title', 'Titulok: ')->setRequired();
+			$form->addTextArea('content', 'Obsah: ')->setRequired();
+
+			$form->addSubmit('send', 'Ulozit a publikovat');
+			$form->onSuccess[] = $this->postFormSucceeded;
+
+			return $form;
+		}
+	}
+
+	// Ukladanie noveho prispevku z forumulara
+	public function postFormSucceeded($form)
+	{
+		$values = $form->getValues();
+		$id = $this->getParameter("id");
+
+		if ($id) {
+			$contents = $this->database->table('blog_content')->get($id);
+			$contents->update($values);
+		} else {
+			$contents = $this->database->table('blog_content')->insert($values);
+		}
+
+		$this->flashMessage("Prispevok bol uspesne publikovany.", 'success');
+		$this->redirect('show', $contents->id);
+	}
+
+	// Editacia prispevku
+	public function actionEdit($id)
+	{
+		if (!$this->user->isloggedIn()) {
+			$this->error('Pre pridavanie alebo editaovanie prispevku musite byt prihlaseny.');
+		} else {
+			$contents = $this->database->table('blog_content')->get($id);
+			if (!$contents) {
+				$this->error('Prispevok nebol najdeny');
+			}
+			$this['postForm']->setDefaults($contents->toArray());
+		}
 	}
 
 }
